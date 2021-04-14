d90f02c651f98af436dd93bab065e07dddfc317c
otaviokochi <otaviokochi@gmail.com>
2021-04-14 19:29:45 -0300

    redirecionando caso token expire

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd'
 import Login from './views/login/Login'
-import store, { loginUser } from './redux/auth';
-
+import store, { loginUser, logoffUser } from './redux/auth';
 import Content from './components/layout/Content';
-import MenuHeader from './components/layout/MenuHeader'
-import SideBar from './components/layout/SideBar'
+import MenuHeader from './components/layout/MenuHeader';
+import SideBar from './components/layout/SideBar';
+import axios from 'axios'
 
 
 const App = () => {
-  const { Header } = Layout 
-  const { loggedIn } = store.getState();
-
+  const { Header } = Layout
+  const  [ loggedIn, setLoggedIn ] = useState(store.getState().loggedIn);
+  
   useEffect(() => {
+    async function validateToken (user) {
+      const url = `http://localhost:5000/validateToken`;
+      return await axios({
+        method: 'POST',
+        url: url,
+        data: { ...user }
+      })
+    }
     const user = JSON.parse(localStorage.getItem('credentials'));
-    if(user) store.dispatch(loginUser(user));
+    if (user) {
+      validateToken(user)
+        .then(_ => {
+          store.dispatch(loginUser(user));
+        })
+        .catch(error => {
+          console.log(error);
+          store.dispatch(logoffUser());
+          setLoggedIn(false);
+        });
+    }
   }, [])
 
   return (
@@ -26,18 +44,18 @@ const App = () => {
           <Login></Login>
         </Route>
         <Route path='/'>
-          {loggedIn ? <Redirect to="/"/> : <Redirect to='/login'/>}
-        <Layout>
-          <Header>
-            <MenuHeader />
-          </Header>
+          {loggedIn ? <Redirect to="/" /> : <Redirect to='/login' />}
           <Layout>
-            <SideBar />
+            <Header>
+              <MenuHeader />
+            </Header>
             <Layout>
-              <Content />
+              <SideBar />
+              <Layout>
+                <Content />
+              </Layout>
             </Layout>
           </Layout>
-        </Layout>
         </Route>
       </Switch>
     </Router>
